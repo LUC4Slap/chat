1994b089708ca8d0415e385bfe8e1e2215669195
lucas_petrolli <lucas.almeida@somoskratos.com.br>
2020-01-09 22:11:44 -0400

    Validação do campo apelido ou nome e dispara alerta caso esteja fora dos criterios

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,16 @@
 module.exports.iniciaChat = (application, req, res) => {
+  let dadosForm = req.body;
+
+  req.assert('apelido', 'Apelido ou Nome obrigatorio').notEmpty();
+  req
+    .assert('apelido', 'Apelido ou Nome tem que ter entre 3 e 15 caracteres')
+    .len(3, 15);
+
+  let erros = req.validationErrors();
+
+  if (erros) {
+    res.render('index', { validacao: erros });
+    return;
+  }
   res.render('chat');
 };
